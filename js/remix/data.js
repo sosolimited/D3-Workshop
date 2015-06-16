025d653b6440433d0db2c86c634b6cd2d38e92d9
JC Nesci <jc.nesci@gmail.com>
2015-06-16 16:00:59 -0700

    Added starting code in remix project so people have a starting point.

---
+++ js/remix/data.js
@@ -0,0 +1,25 @@
+// Use Queue.js (by Bostock) to load & combine data from various API calls.
+var q = queue();
+
+var dataUrls =  [ "http://www.omdbapi.com/?t=Akira&y=&plot=short&r=json&tomatoes=true",
+									"http://www.omdbapi.com/?t=Ghost In The Shell&y=&plot=short&r=json&tomatoes=true",
+									"http://www.omdbapi.com/?t=Power Rangers&y=&plot=short&r=json&tomatoes=true",
+									"http://www.omdbapi.com/?t=Caligula&y=&plot=short&r=json&tomatoes=true",
+									"http://www.omdbapi.com/?t=Hot Tub Time Machine&y=&plot=short&r=json&tomatoes=true" ];
+
+dataUrls.forEach( function( d ) { q.defer( d3.json, d ); });
+
+q.awaitAll( function( error, results ){
+
+	if (error) {
+
+		console.warn( "ERROR loading files", error );
+
+	} else {
+
+		console.log( "results:" );
+		console.log( results );
+
+	}
+
+});
